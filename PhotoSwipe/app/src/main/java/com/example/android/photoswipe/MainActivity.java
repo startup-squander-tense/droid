829cd4aa570a1be57f80f3acac46e40ccc3aafbe
fakas <asdf@asdfsf.com>
2017-10-03 20:41:39 -0700

    Add files, fix buttons, add logic to read all files

--- PhotoSwipe/app/src/main/java/com/example/android/photoswipe/MainActivity.java
+++ PhotoSwipe/app/src/main/java/com/example/android/photoswipe/MainActivity.java
@@ -1,19 +1,24 @@
 package com.example.android.photoswipe;
 
 import android.content.res.AssetFileDescriptor;
+import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     Button mButtonPlayButton;
     Button mButtonStopButton;
+    ImageView mHeaderImage;
+    ArrayList<String> files;
 
     MediaPlayer player;
 
@@ -22,10 +27,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        try {
+            mHeaderImage = (ImageView) findViewById(R.id.imageViewHeaderImage);
+            mHeaderImage.setImageBitmap(BitmapFactory.decodeStream(getAssets().open("langzone.png")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         mButtonPlayButton = (Button) findViewById(R.id.buttonPlayButton);
         mButtonPlayButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                playMp3AtPath("wowow1.mp3");
+                playMp3AtPath("lang/wowow1.mp3");
             }
         });
 
@@ -36,13 +48,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        files = readAllFilesInAssets("lang");
     }
+
+    public ArrayList<String> readAllFilesInAssets(String path) {
+        String[] list;
+        ArrayList<String> files = new ArrayList<String>();
+
+        try {
+            list = getAssets().list(path);
+            if (list.length > 0) {
+                // This is a folder
+                for (String file : list) {
+                    files.add(file);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return files;
+    }
+
     private void stopPlayingIfPlaying() {
         if (player == null) {
             return;
         }
 
-        if(player.isPlaying()) {
+        if (player.isPlaying()) {
             player.stop();
         }
     }
@@ -53,10 +86,10 @@ public class MainActivity extends AppCompatActivity {
 
             AssetFileDescriptor afd = getAssets().openFd(path);
             player = new MediaPlayer();
-            player.setDataSource(afd.getFileDescriptor(),afd.getStartOffset(),afd.getLength());
+            player.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
             player.prepare();
             player.start();
-            return  true;
+            return true;
 
         } catch (IOException e) {
             e.printStackTrace();
--- PhotoSwipe/app/src/test/java/com/example/android/photoswipe/ExampleUnitTest.java
+++ PhotoSwipe/app/src/test/java/com/example/android/photoswipe/ExampleUnitTest.java
@@ -2,6 +2,8 @@ package com.example.android.photoswipe;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 /**
@@ -13,5 +15,13 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void readFilesWorks() {
+//        MainActivity activity = new MainActivity();
+//        ArrayList<String> files = activity.readAllFilesInAssets("");
+        assertEquals(1 + 1, 2);
+//        assertEquals(files.contains("wowow1.mp3"), true);
     }
 }
