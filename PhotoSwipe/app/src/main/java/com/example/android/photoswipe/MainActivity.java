d7911763af2a6e72d898224a92a842ac471a7e7b
Pete Browning <aquacadet@gmail.com>
2017-10-03 19:58:17 -0700

    Add buttons and sound file

--- PhotoSwipe/app/src/main/java/com/example/android/photoswipe/MainActivity.java
+++ PhotoSwipe/app/src/main/java/com/example/android/photoswipe/MainActivity.java
@@ -1,13 +1,63 @@
 package com.example.android.photoswipe;
 
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mButtonPlayButton;
+    Button mButtonStopButton;
+
+    MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtonPlayButton = (Button) findViewById(R.id.buttonPlayButton);
+        mButtonStopButton = (Button) findViewById(R.id.buttonStop);
+
+        mButtonPlayButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                playMp3AtPath("lang.mp3");
+            }
+        });
+
+        mButtonPlayButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                stopPlayingIfPlaying();
+            }
+        });
+
+    }
+    private void stopPlayingIfPlaying() {
+        if(player != null && player.isPlaying()) {
+            player.stop();
+        }
+    }
+
+    private boolean playMp3AtPath(String path) {
+        try {
+//            stopPlayingIfPlaying();
+
+            AssetFileDescriptor afd = getAssets().openFd(path);
+            player = new MediaPlayer();
+            player.setDataSource(afd.getFileDescriptor(),afd.getStartOffset(),afd.getLength());
+            player.prepare();
+            player.start();
+            return  true;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
